7487f800b87dab73357cb64039003ed6269ec844
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-13 00:47:42 -0500

    shadcn input install

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,9 @@
 const SearchBar = () => {
     return ( 
-        <div>
-            
+        <div className='relative w-full h-14 flex flex-col bg-white'>
+            <div className='h-14 relative z-10 rounded-md'>
+                
+            </div>
         </div>
     )
 }
