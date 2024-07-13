2efdd386114132ca3f9d4859a98b963b48ebaeda
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-13 00:46:17 -0500

    searchbar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SearchBar from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
