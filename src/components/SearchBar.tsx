5a9cb337b345e0958a46ec46065c48adbdecd67f
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-13 00:58:56 -0500

    added accessibility feature to search component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,25 @@
+'use client'
+
 import { Search } from "lucide-react"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
+import { useRef } from "react"
 
 const SearchBar = () => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
     return ( 
         <div className='relative w-full h-14 flex flex-col bg-white'>
             <div className='h-14 relative z-10 rounded-md'>
-                <Input className='absolute inset-0 h-full' />
+                <Input 
+                    onKeyDown={(e) => {
+                        if(e.key === "Escape") {
+                            inputRef?.current?.blur()
+                        }
+                    }}
+                    ref={inputRef} 
+                    className='absolute inset-0 h-full' />
                 <Button
                     className='absolute right-0 inset-y-0 h-full rounded-l-none'
                 >
