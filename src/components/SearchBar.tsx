71a32db5110299f54722a3bc09a8a2509d492f7a
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-13 00:48:47 -0500

    search bar design

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,10 @@
+import { Input } from "./ui/input"
+
 const SearchBar = () => {
     return ( 
         <div className='relative w-full h-14 flex flex-col bg-white'>
             <div className='h-14 relative z-10 rounded-md'>
-                
+                <Input className='absolute inset-0 h-full' />
             </div>
         </div>
     )
